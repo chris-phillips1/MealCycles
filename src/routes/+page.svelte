ac6a9d29a6cd1e2601e1bc12b9330c50ad374d1b
Chris Phillips <cpphillips13@gmail.com>
2026-02-21 19:36:14 -0500

    Add cycle updater to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,56 @@
 <script lang="ts">
-	import { getCycle, getIngredients, getMealPlans, getMeals } from '$lib/stores/state.svelte';
+	import {
+		getCycle,
+		getIngredients,
+		getMealPlans,
+		getMeals,
+		updateCycle
+	} from '$lib/stores/state.svelte';
+
+	let formData = {
+		startDate: getCycle().startDate,
+		cycleLength: getCycle().cycleLength
+	};
+
+	function onsubmit(event: Event) {
+		event.preventDefault();
+		updateCycle({ startDate: formData.startDate, cycleLength: formData.cycleLength });
+	}
 </script>
 
 <h1>Data Models</h1>
 <div class="data-model">
-	<div class="data-model-item cycle">
+	<div class="data-model-item">
 		<h2>Cycle</h2>
 		<pre>{JSON.stringify(getCycle(), null, 2)}</pre>
 	</div>
 
-	<div class="data-model-item ingredients">
+	<div class="data-model-item edit">
+		<h2>Edit Cycle</h2>
+		<form {onsubmit}>
+			<div>
+				<label for="cycle-date">Cycle Start:</label>
+				<input id="cycle-date" type="date" bind:value={formData.startDate} />
+			</div>
+			<div>
+				<label for="cycle-length">Cycle Length:</label>
+				<input id="cycle-length" type="number" bind:value={formData.cycleLength} />
+			</div>
+			<input type="submit" />
+		</form>
+	</div>
+
+	<div class="data-model-item">
 		<h2>Ingredients</h2>
 		<pre>{JSON.stringify(getIngredients(), null, 2)}</pre>
 	</div>
 
-	<div class="data-model-item meals">
+	<div class="data-model-item">
 		<h2>Meals</h2>
 		<pre>{JSON.stringify(getMeals(), null, 2)}</pre>
 	</div>
 
-	<div class="data-model-item mealPlans">
+	<div class="data-model-item">
 		<h2>Meal Plans</h2>
 		<pre>{JSON.stringify(getMealPlans(), null, 2)}</pre>
 	</div>
@@ -40,5 +71,11 @@
 		background-color: #f0f0f0;
 		padding: 1rem;
 		border-radius: 8px;
+	}
+
+	.data-model-item.edit form {
+		gap: 0.5rem;
+		align-items: flex-start;
+		justify-content: flex-start;
 	}
 </style>
